8bc46c4d73038df8f8887b9b8cf96d626b4c593c
Vladimir Rastov <vladimir@rastov.ru>
2019-10-07 00:20:41 +0400

    Добавил bootstrap-vue и сам bootstrap, подключил css спомощью function loadCss(url) в main.js, попробоал подключить Vue.use(BootstrapVue); падает... `Uncaught TypeError: Vue.extend is not a function`

--- src/main/resources/public/app/main.js
+++ src/main/resources/public/app/main.js
@@ -1,11 +1,24 @@
 (function () {
     'use strict';
 
+    function loadCss(url) {
+        var link = document.createElement("link");
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.href = url;
+        document.getElementsByTagName("head")[0].appendChild(link);
+    }
+
+    loadCss("css/bootstrap/4.3.1/bootstrap.css");
+    loadCss("scripts/bootstrap-vue/2.0.2/bootstrap-vue.css");
+
     requirejs.config({"paths": {"Vue": '/scripts/vue/2.6.10/vue'}});
     requirejs.config({"paths": {"vue": ["/scripts/require-vuejs/1.1.3/require-vuejs"]}});
+    requirejs.config({"paths": {"bootstrap-vue": ["/scripts/bootstrap-vue/2.0.2/bootstrap-vue"]}});
 
-    define(["Vue", "vue!App"],
-        function (Vue) {
+    define(["Vue", "bootstrap-vue", "vue!App"],
+        function (Vue, BootstrapVue) {
+            Vue.use(BootstrapVue);
             new Vue({
                 el: '#app',
                 template: '<App/>'
