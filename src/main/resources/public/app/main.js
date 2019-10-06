9353b208d012c74bfa5cacb0111d2d79d4c09601
Vladimir Rastov <vladimir@rastov.ru>
2019-10-06 21:48:16 +0300

    Добавил билиотеку require-vuejs. Добавлен первый однофайловый компонент Vue - App.vue который используется в main.js. README.md обновленны.

--- src/main/resources/public/app/main.js
+++ src/main/resources/public/app/main.js
@@ -2,12 +2,13 @@
     'use strict';
 
     requirejs.config({"paths": {"Vue": '/webjars/vue/2.6.11/vue'}});
+    requirejs.config({"paths": {"vue": ["/scripts/require-vuejs/1.1.3/require-vuejs"]}});
 
-    define(["Vue"],
+    define(["Vue", "vue!App"],
         function (Vue) {
             new Vue({
                 el: '#app',
-                template: '<div>Hello Vue!</div>'
+                template: '<App/>'
             });
         }
     );
